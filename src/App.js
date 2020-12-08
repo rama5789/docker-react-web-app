7ba31b7a8b57427515880a2f1989011ea865e920
Rama Chandra Nayak <purna.july10@gmail.com>
2020-12-08 12:57:53 +0530

    8th commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn Elastic Beanstalk
+          Learn React
         </a>
       </header>
     </div>
